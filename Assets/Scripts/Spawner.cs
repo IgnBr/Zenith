54b7f88ddae59fb8e77a4a8980b8192c7a1aa49b
IgnBr <71457794+IgnBr@users.noreply.github.com>
2021-11-24 18:55:23 +0200

    dsadad

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -5,17 +5,31 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     private Tree treeScript;
+    public GameObject golem;
+    private bool spawned = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        treeScript = GetComponent<Tree>();
+        treeScript = FindObjectOfType<Tree>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (treeScript.lightOfTree)
+        if (!treeScript.lightOfTree)
         {
+            spawned = false;
+        }
+
+        if (treeScript.lightOfTree && !spawned)
+        {
+            spawned = true;
+            Vector3 vector = treeScript.transform.position;
+            vector.x += 20;
+            vector.z += 20;
+            vector.y += 20;
+            Instantiate(golem, vector, Quaternion.identity);
         }
     }
 }
